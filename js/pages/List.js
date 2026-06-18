c82281a98085a2479bed5657b6a27a65691ca9c4
picreatoragain <logancssavinkoff@gmail.com>
2026-06-18 13:28:23 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,6 +54,10 @@ export default {
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
+                            <li>
+                            <div class="type-title-sm">Tags</div>
+                            <p>{{ level.tags }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
