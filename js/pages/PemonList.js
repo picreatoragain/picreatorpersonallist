912c463ab4e52bafbcc3c622379ddd0f3bad8c8e
picreatoragain <logancssavinkoff@gmail.com>
2026-07-27 19:40:50 -0600

    Create PemonList.js

---
+++ js/pages/PemonList.js
@@ -0,0 +1,154 @@
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../pemoncontent.js";
+
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
+
+const roleIconMap = {
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
+};
+
+export default {
+    components: { Spinner, LevelAuthors },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-list">
+            <div class="list-container">
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 37" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy Completions</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="level-container">
+                <div class="level" v-if="level">
+                    <h1>{{ level.name }}</h1>
+                    <h2> Published by {{ level.author }}</h2>
+                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ level.id }}</p>
+                        </li>
+                            <li>
+                            <div class="type-title-sm">Tags</div>
+                            <p>{{ level.tags }}</p>
+                        </li>
+                         <li>
+                            <div class="type-title-sm">Attempts</div>
+                            <p>{{ level.attempts }}</p>
+                        </li>
+                       <li>
+                            <div class="type-title-sm">Difficulty Opinion</div>
+                            <p>{{ level.difficulty }}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                </div>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>Notes and info.</h3>
+                    <p>
+                        This is NOT the pipislist, please give submissions for your levels at https://pipis-list.vercel.app/#/ This is just for me to put my personal completions in
+                    </p>
+                    <p>
+                        Some demon levels ive beaten (poltergeist) I dont have video recordings for so I just use a showcase.
+                    </p>
+                    <p>
+                        The niche memes are mainstream.
+                    </p>
+                </div>
+            </div>
+        </main>
+    `,
+    data: () => ({
+        list: [],
+        editors: [],
+        loading: true,
+        selected: 0,
+        errors: [],
+        roleIconMap,
+        store
+    }),
+    computed: {
+        level() {
+            return this.list[this.selected][0];
+        },
+        video() {
+            if (!this.level.showcase) {
+                return embed(this.level.verification);
+            }
+
+            return embed(
+                this.toggledShowcase
+                    ? this.level.showcase
+                    : this.level.verification
+            );
+        },
+    },
+    async mounted() {
+        // Hide loading spinner
+        this.list = await fetchList();
+        this.editors = await fetchEditors();
+
+        // Error handling
+        if (!this.list) {
+            this.errors = [
+                "wakeup picreator bro.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => {
+                        return `Failed to load level. (${err}.json)`;
+                    })
+            );
+            if (!this.editors) {
+                // this.errors.push("Failed to load list editors.");
+                // I dont feel like removing editor list properly so fuck you
+            }
+        }
+
+        this.loading = false;
+    },
+    methods: {
+        embed,
+        score,
+    },
+};
