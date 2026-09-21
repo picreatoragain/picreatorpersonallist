a169ad09115e4dda42c0f0ba4d94b5492e4f1617
picreatoragain <logancssavinkoff@gmail.com>
2026-09-21 15:17:12 -0600

    Update skillsets.js

--- data/skillsets.js
+++ data/skillsets.js
@@ -2,7 +2,7 @@ export default {
     Ship: "Levels with most of the difficulty in the ship sections.",
     Wave: "Levels with most of the difficulty in the wave sections.",
     Unbalanced: "The difficulty in this level is NOT consistent.",
-    Fast-Paced: "Most of this levels gameplay/difficulty is in fast parts.",
-    Slow-Paced: "Most of this levels gameplay/difficulty is in slow parts.",
+    "Fast-Paced": "Most of this levels gameplay/difficulty is in fast parts.",
+    "Slow-Paced": "Most of this levels gameplay/difficulty is in slow parts.",
     Timings: "Levels that want you to do precise timings NOW!!!!!"
 };
