15b521aead5d22eaa3502872110f175897a4b86f
picreatoragain <logancssavinkoff@gmail.com>
2026-09-21 15:07:11 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -2,7 +2,7 @@ import { store } from "../main.js";
 import { embed } from "../util.js";
 import { score } from "../score.js";
 import { fetchEditors, fetchList } from "../content.js";
-import skillDescriptions from "../data/skillsets.js";
+import skillDescriptions from "../../data/skillsets.js";
 import Spinner from "../components/Spinner.js";
 import LevelAuthors from "../components/List/LevelAuthors.js";
 
