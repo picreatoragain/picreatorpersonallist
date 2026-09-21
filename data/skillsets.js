20e60b741b9bfdb20894da57d0e38c4d7d627cd3
picreatoragain <logancssavinkoff@gmail.com>
2026-09-21 15:27:46 -0600

    Update skillsets.js

--- data/skillsets.js
+++ data/skillsets.js
@@ -1,6 +1,8 @@
 export default {
     Ship: "Levels with alot of the difficulty in the ship sections.",
     Wave: "Levels with alot of the difficulty in the wave sections.",
+    Cube: "Levels with alot of the difficulty in the cube sections.",
+    Spider: "Levels that SOMEHOW have alot of the difficulty in the spider sections.",
     Unbalanced: "The difficulty in this level is NOT consistent.",
     Duals: "The worst gamemode.",
     "Fast-Paced": "Most of this levels gameplay/difficulty is in fast parts.",
