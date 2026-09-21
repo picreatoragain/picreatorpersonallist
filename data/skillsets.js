a986865f39e5341424311fd7fc8d818026084534
picreatoragain <logancssavinkoff@gmail.com>
2026-09-21 16:30:09 -0600

    Update skillsets.js

--- data/skillsets.js
+++ data/skillsets.js
@@ -5,6 +5,7 @@ export default {
     Spider: "Levels that SOMEHOW have alot of the difficulty in the spider sections.",
     Unbalanced: "The difficulty in this level is NOT consistent.",
     Duals: "The worst gamemode.",
+    "Take a WILD guess": "Its wave.",
     "Fast-Paced": "Most of this levels gameplay/difficulty is in fast parts.",
     "Slow-Paced": "Most of this levels gameplay/difficulty is in slow parts.",
     "Nerve Control": "Long + Intense equals sweat.",
