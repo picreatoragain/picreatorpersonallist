25114b6214619d1870ce0462ea916dd7e1f6bdf4
picreatoragain <logancssavinkoff@gmail.com>
2026-09-21 15:14:27 -0600

    Update skillsets.js

--- data/skillsets.js
+++ data/skillsets.js
@@ -1,5 +1,6 @@
 export default {
-    ship: "test",
-    wave: "test",
-    timings: "test"
+    Ship: "Levels with most of the difficulty in the ship sections.",
+    Wave: "Levels with most of the difficulty in the wave sections.",
+    Unbalanced: "The difficulty in this level is NOT consistent.",
+    Timings: "Levels that want you to do precise timings NOW!!!!!"
 };
